cba898427faa04b6398cc842e56c6db2ba525f03
muhammed  arshaq tk <arshaqtk4@gmail.com>
2026-08-05 12:52:12 +0530

    feat: add ProductsHeroBanner component with furniture category cards

--- src/components/products/ProductsHeroBanner.jsx
+++ src/components/products/ProductsHeroBanner.jsx
@@ -38,7 +38,7 @@ const categoryCards = [
     id: "furniture",
     name: "FURNITURE",
     filterKey: "Furniture",
-    image: "/product/categories/furniture.png",
+    image: "/product/categories/sofa.jpeg",
   },
 ];
 
